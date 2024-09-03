2003e1773a46481f45f4480cd9431e9468ca020c
Anarikke <anla0008@stud.kea.dk>
2024-09-03 08:09:12 +0200

    produkt + liste

--- js/produktliste.js
+++ js/produktliste.js
@@ -19,6 +19,7 @@ function showProduct(product) {
     // produktet er udsolgt
     copy.querySelector("article").classList.add("soldOut");
   }
+  copy.querySelector(".read-more").setAttribute("href", `produkt.html?id=${product.id}`);
   // append
   document.querySelector("main").appendChild(copy);
 }
